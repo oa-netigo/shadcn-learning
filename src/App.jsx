edbbed9245d2899d08937fc7096cea0b33f07c8b
Omar Abdullah <omar.abdullah@netigo.de>
2025-09-24 11:10:46 +0200

    feat: add Accordion component and demo, update package.json and pnpm-lock.yaml

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button"
+import { AccordionDemo } from "@/demos/Accordion"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="container mx-auto">
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+        {/* <AccordionDemo /> */}
+      </div>
     </div>
   )
 }
